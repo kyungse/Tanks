fc76ede15ed0ca24e9e595b525e50bb1a15fa5ea
System Administrator <root@baggyeongse-ui-MacBook-Air.local>
2016-10-27 16:20:17 -0400

    TanksHealthDone

---
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;
+    public ParticleSystem m_ExplosionParticles;       
+    public AudioSource m_ExplosionAudio;              
+    public float m_MaxDamage = 100f;                  
+    public float m_ExplosionForce = 1000f;            
+    public float m_MaxLifeTime = 2f;                  
+    public float m_ExplosionRadius = 5f;              
+
+
+    private void Start()
+    {
+		// If it isn't destoryed by then, destroy the shell after it's lifetime.
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+		// Collect all the colliders in a sphere from the shell's current position to a radius of the explosion radius.
+		Collider[] colliders = Physics.OverlapSphere (transform.position, m_ExplosionRadius, m_TankMask);
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        // Calculate the amount of damage a target should take based on it's position.
+        return 0f;
+    }
+}
